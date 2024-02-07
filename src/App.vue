6327859831ad71ac8a295a179965afdf27d618d2
tribuSimo <tribusimo@gmail.com>
2024-02-07 09:58:39 +0100

    creata infrastruttura pagina login

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <login></login>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import login from './components/login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    login
   }
 }
 </script>
---
+++ src/components/login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+    <v-card class="login-card" elevation="4">
+      <v-card-title>Login</v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent="login">
+          <v-text-field
+            v-model="email"
+            label="Email"
+            outlined
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            outlined
+            required
+            type="password"
+          ></v-text-field>
+          <v-btn type="submit" color="primary">Login</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+    </div>
+  </template>
+
+<script>
+import '../assets/style/style.css';
+export default {
+    name: 'login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      // Implementazione del login
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+    }
+  }
+};
+</script>
