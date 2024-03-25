470886838c2514a356c3bc35189cec5da126b5db
tribuSimo <tribusimo@gmail.com>
2024-03-25 09:29:07 +0100

    interazione con il backend per visualizzare i dati delle auto

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,32 +1,34 @@
-<!-- eslint-disable vue/no-unused-components -->
 <template>
   <v-app>
     <navbar></navbar>
     <v-container class="containerSearch">
-        <v-col v-for="index in 5" :key="index" cols="12" class="text-left">
-          <v-card outlined>
-            <v-container>
-              <v-row>
-                <v-col cols="12">
-                  <v-label>Etichetta {{ index }}</v-label>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="12">
-                  <v-combobox
-                    v-model="selectedItem"
-                    :items="items"
-                    label="Seleziona un elemento"
-                  ></v-combobox>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card>
-        </v-col>
+      <v-col v-for="(auto, index) in auto" :key="index" cols="12" class="text-left">
+        <v-card outlined>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-label>Modello: {{ auto.modello }}</v-label>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-label>Prezzo: {{ auto.prezzo }}</v-label>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-label>Anno di produzione: {{ auto.annoProduzione }}</v-label>
+              </v-col>
+            </v-row>
+            <!-- Aggiungi qui altre informazioni sull'auto -->
+          </v-container>
+        </v-card>
+      </v-col>
     </v-container>
     <finePagina></finePagina>
   </v-app>
 </template>
+
 
 <script>
 import navbar from './navbar.vue';
@@ -40,14 +42,39 @@ export default {
     return {
       selectedItem: null,
       items: ['Elemento 1', 'Elemento 2', 'Elemento 3', 'Elemento 4', 'Elemento 5'],
+      auto: null,
     };
   },
+  created() {
+    this.caricaAuto();
+  },
+  methods: {
+    async caricaAuto() {
+      const token = localStorage.getItem('token');
+      try {
+        const response = await fetch('http://localhost:3000/api/auto', {
+          method: 'GET',
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            'Authorization': `${token}`
+          }
+        });
+        if (response.ok) {
+          this.auto = await response.text();
+          console.log('Ecco le auto: ', this.auto);
+        } else
+          console.error('Errore nel caricamento delle auto:', response.statusText);
+
+      } catch (error) {
+        console.error('Errore nel caricamento delle auto:', error);
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
-.containerSearch
-{
+.containerSearch {
   width: 25%;
   height: 800px;
   margin-left: 0px;
@@ -56,9 +83,9 @@ export default {
   float: right;
 }
 
-.containerMain{
+.containerMain {
   width: 75%;
-  float: left ;
+  float: left;
   margin-top: 0px;
   margin-left: 0px;
   position: absolute;
