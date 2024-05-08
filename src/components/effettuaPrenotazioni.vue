ff25e56a61ca16a54e2a2a5893c1a556920fef36
tribuSimo <tribusimo@gmail.com>
2024-05-08 08:14:17 +0200

    tolto button non voluto

--- src/components/effettuaPrenotazioni.vue
+++ src/components/effettuaPrenotazioni.vue
@@ -22,9 +22,6 @@
 
     <!-- Dialog -->
     <v-dialog v-model="dialogVisibile" max-width="600">
-      <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-      </template>
       <v-card>
         <v-card-title class="headline">Scegli un Orario</v-card-title>
         <v-card-text>
