2229cd536a56adce80a8670804a0d201ec9c6301
tribuSimo <tribusimo@gmail.com>
2024-06-22 19:15:06 +0200

    iniziata gestione vendita auto

--- src/components/admin/dettagliAuto.vue
+++ src/components/admin/dettagliAuto.vue
@@ -1,161 +1,110 @@
 <template>
-    <v-app>
-      <navbar></navbar>
-      <v-container class="containerSearch">
-        <p></p>
-      </v-container>
-      <finePagina></finePagina>
-    </v-app>
-  </template>
-  <script>
-  import navbar from './navbar.vue';
-  import finePagina from '../footer.vue';
-  import router from '@/router';
-  
-  export default {
-    components: {
-      navbar,
-      finePagina
-    },
-    data() {
-      return {
-        auto: [],
-        mostraUsate: false,
-        marche: [],
-        idUtente: null,
-        errorMessage: '',
-        inner_filtro: "Nessuno",
-        inner_marca: "Nessuna",
-        cercaQuery: ""
-      };
-    },
-    created() {
-      if (localStorage.getItem('token')  && localStorage.getItem('ruolo') && localStorage.getItem('ruolo') >= 1) {
-        this.caricaAuto();
-        this.visualizzaMarche();
-      } else {
-        router.push('/login');
-      }
-    },
-    watch: {
-      mostraUsate(newValue, oldValue) {
-        if (newValue !== oldValue) {
-          this.visualizzaUsate();
-        }
-      }
-    },
-    computed: {
-      filtro: {
-        get() {
-          return this.inner_filtro;
-        },
-        set(val) {
-          this.inner_filtro = val;
-          this.caricaAuto();
-        }
-      },
-      marcaSelezionata: {
-        get() {
-          return this.inner_marca;
-        },
-        set(val) {
-          this.inner_marca = val;
-          this.caricaAuto();
-        }
-      }
-    },
-    methods: {
-      prendiCerca(searchQuery){
-        this.cercaQuery = searchQuery;
-        this.caricaAuto();
-      },
-      getImageSrc(auto) {
-        if (auto.immagini && auto.immagini.length > 0) {
-          // Separa la stringa delle immagini e prendi la prima immagine
-          const images = auto.immagini.split(',');
-          // Aggiungi il percorso corretto della cartella pubblica
-          return images.length > 0 ? `/${images[0]}` : '/workInProgress.jpg';
-        }
-        // Restituisce un'immagine di fallback se non ci sono immagini
-        return '/workInProgress.jpg';
-      },
-      async caricaAuto() {
-        try {
-          const token = localStorage.getItem('token');
-          let url = `${window.dreamdrive_cfg.api}/auto`;
-          const params = new URLSearchParams();
-          if (this.filtro && this.filtro !== "Nessuno") {
-            params.append('sortBy', this.filtro);
-          }
-          console.log(this.cercaQuery);
-          if(this.cercaQuery && this.cercaQuery !== ""){
-            params.append('cerca', this.cercaQuery);
-          }
-          if (this.mostraUsate) {
-            params.append('usata', 'true');
-          }
-  
-          if (this.marcaSelezionata && this.marcaSelezionata !== 'Nessuna') {
-            params.append('marca', this.marcaSelezionata);
-          }
-  
-          if (params.toString()) {
-            url += `?${params.toString()}`;
-          }
-  
-          const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-              'Authorization': `${token}`
-            }
-          });
-  
-          if (response.ok) {
-            this.auto = await response.json();
-          } else {
-            console.error('Errore nel caricamento delle auto:', response.statusText);
-            this.errorMessage = 'Errore nel caricamento delle auto: ' + response.statusText;
-          }
-        } catch (error) {
-          console.error('Errore nel caricamento delle auto:', error);
-          this.errorMessage = 'Errore nella richiesta delle auto: ' + error.message;
-        }
-      },
-      visualizzaUsate() {
-        this.caricaAuto();
-      },
-      async visualizzaMarche() {
-        try {
-          const token = localStorage.getItem('token');
-          const response = await fetch(`${window.dreamdrive_cfg.api}/marche`, {
-            method: 'GET',
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded",
-              'Authorization': `${token}`
-            }
-          });
-          if (response.ok) {
-            const marcheJson = await response.json();
-            // Aggiungi la marca "Nessuna" all'inizio dell'array
-            this.marche = ['Nessuna', ...marcheJson.map(marca => marca.marca)];
-          } else {
-            console.error('Errore nel caricamento delle marche:', response.statusText);
-            this.errorMessage = 'Errore nel caricamento delle marche: ' + response.statusText;
-          }
-        } catch (error) {
-          console.error('Errore nel caricamento delle marche:', error);
-          this.errorMessage = 'Errore nella richiesta delle marche: ' + error.message;
-        }
-      },
-      visualizzaDettagli(index) {
-        const idAuto = this.auto[index].idAuto;
-        this.$router.push({ name: 'Dettagli auto', params: { idAuto: idAuto } });
-      }
+  <v-app>
+    <navbar></navbar>
+    <v-container class="container">
+      <v-alert title="Errore" v-if="errorMessage" color="error" closable>
+        {{ errorMessage }}
+      </v-alert>
+      <v-row v-if="auto.length > 0">
+        <v-col cols="12">
+          <v-card outlined>
+            <v-carousel v-if="auto[0].immagini != null">
+              <v-carousel-item v-for="(image, index) in auto[0].immagini.split(',')" :key="index">
+                <v-img :src="`/${image.trim()}`" aspect-ratio="16/9"></v-img>
+              </v-carousel-item>
+            </v-carousel>
+            <p v-else>Immagine non disponibile</p>
+            <v-card-title>{{ auto[0].marca }} {{ auto[0].modello }}</v-card-title>
+            <v-card-text>
+              <div>Potenza: {{ auto[0].potenza }} cv</div>
+              <div>Chilometraggio: {{ auto[0].chilometraggio }} km</div>
+              <div>Anno di produzione: {{ auto[0].annoProduzione }}</div>
+              <div>Usata: {{ auto[0].usata ? 'Sì' : 'No' }}</div>
+              <div>Prezzo: {{ auto[0].prezzo }} €</div>
+              <div>Carburante: {{ auto[0].carburante }}</div>
+              <div>Colore: {{ auto[0].colore }}</div>
+              <div>Colore: {{ auto[0].colore }}</div>
+              <v-btn @click="effettuaVendita()" class="btnDetails" block>Effettua vendita</v-btn>
+              <v-btn @click="returnCatalog()" class="btnDetails" block>Ritorna al catalogo</v-btn>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row v-else>
+        <v-col cols="12">
+          <p>Nessun dettaglio dell'auto disponibile.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+    <finePagina></finePagina>
+  </v-app>
+</template>
+
+<script>
+import navbar from './navbar.vue';
+import finePagina from '../footer.vue';
+import router from '@/router';
+
+export default {
+  components: {
+    navbar,
+    finePagina
+  },
+  data() {
+    return {
+      auto: {},
+      errorMessage: ''
+    };
+  },
+  created() {
+    if (localStorage.getItem('token')  && localStorage.getItem('ruolo') && localStorage.getItem('ruolo') >= 2) {
+      this.caricaAuto(this.$route.params.idAuto);
+    } else {
+      router.push('/login');
     }
-  };
-  </script>
-  
-  <style scoped>
-  
-  </style>
+  },
+  methods: {
+    async caricaAuto(idAuto) {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await fetch(`${window.dreamdrive_cfg.api}/auto/${idAuto}`, {
+          method: 'GET',
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            'Authorization': `${token}`
+          }
+        });
+        if (response.ok) {
+          this.auto = await response.json();
+        } else {
+          this.errorMessage = 'Errore nel caricamento dei dettagli dell\'auto: ' + response.statusText;
+        }
+      } catch (error) {
+        this.errorMessage = 'Errore nel caricamento dei dettagli dell\'auto: ' + error.message;
+      }
+    },
+
+    returnCatalog() {
+      router.push("/home");
+    },
+
+    effettuaPrenotazione() {
+      this.$router.push({ name: 'Effettua Prenotazioni', params: { idAuto: this.auto.idAuto } });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  margin-top: 165px;
+  margin-bottom: 95px;
+}
+
+.btnDetails {
+  margin-top: 10px;
+  background-color: #ffa500;
+  color: white;
+  width: 200px;
+}
+</style>
